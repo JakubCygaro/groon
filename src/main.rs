ccada66e8906aa8277b39031375bbc12c7161a2c
papieros <robertrugala@gmail.com>
2025-11-06 21:26:02 +0100

    args parsing finish

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,22 @@ use std::path::{self, PathBuf};
 use std::str::FromStr;
 mod templating;
 
+const DEFAULT_ADRESS: &str = "127.0.0.1";
+const DEFAULT_PORT: u16 = 8080;
+
 struct AppState {
     root_path: path::PathBuf,
     templates: path::PathBuf,
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+struct Args<'a> {
+    address: &'a str,
+    port: u16,
+    wwwroot: String,
+    templates: String,
+}
+
+fn parse_args<'a>() -> Args<'a> {
     let args = clap::Command::new("groon-server")
         .arg(
             clap::Arg::new("address")
@@ -27,40 +36,62 @@ async fn main() -> std::io::Result<()> {
         .arg(
             clap::Arg::new("templates")
                 .env("GROONTEMPLATES")
-                .long("templates-dir"),
+                .short('t')
+                .long("templates-dir")
+                .required(true),
         )
         .arg(
-            clap::Arg::new("website")
-                .env("GROONWWW")
-                .long("website-dir"),
+            clap::Arg::new("wwwroot")
+                .env("GROONWWWROOT")
+                .short('w')
+                .long("wwwroot-dir")
+                .required(true),
         )
         .get_matches();
     let address = args
         .get_one::<&str>("address")
-        .map_or(, |a| &a);
+        .map_or(DEFAULT_ADRESS, |a| a);
     let port = args
         .get_one::<u16>("port")
-        .map_or(defaults::BACKEND_PORT, |p| *p);
-    let wwwroot = std::env::var("WWWROOT").expect("set WWWROOT");
-    let templates = std::env::var("TEMPLATES").expect("set TEMPLATES");
+        .map_or(DEFAULT_PORT, |p| *p);
+    let wwwroot = args
+        .get_one::<String>("wwwroot")
+        .expect("wwwroot not provided")
+        .to_owned();
+    let templates = args
+        .get_one::<String>("templates")
+        .expect("templates directory not provided")
+        .to_owned();
+    Args {
+        address,
+        port,
+        wwwroot,
+        templates
+    }
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let args = parse_args();
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("debug"));
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
             .wrap(Logger::new("%a ${User-Agent}i"))
             .app_data(web::Data::new(AppState {
-                root_path: path::PathBuf::from_str(&wwwroot)
+                root_path: path::PathBuf::from_str(&args.wwwroot)
                     .unwrap()
                     .canonicalize()
                     .unwrap(),
-                templates: path::PathBuf::from_str(&templates)
+                templates: path::PathBuf::from_str(&args.templates)
                     .unwrap()
                     .canonicalize()
                     .unwrap(),
             }))
             .service(serve_files)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((args.address, args.port))?
     .run()
     .await
 }
